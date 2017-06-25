b4d64627bac3d69b034d37fae22df69951aa2b72
Matt Hamrick <matthewjhamrick@gmail.com>
2017-06-25 14:00:07 -0500

    fixed a lil linting error

--- src/components/info/teams.jsx
+++ src/components/info/teams.jsx
@@ -4,8 +4,6 @@ import {
   connect,
 } from 'react-redux'
 
-import Forfeit from './forfeit.jsx'
-
 import {
   fgColorForRGB,
   hexToRGB,
@@ -15,6 +13,8 @@ import {
   backgroundColorPath,
   currentTeamPath,
 } from '../../redux/paths.js'
+
+import Forfeit from './forfeit.jsx'
 
 import './teams.css'
 
