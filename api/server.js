a359a58a82a390631a5219b1aec69487b3021221
Matt <matthewjhamrick@gmail.com>
2017-12-16 14:50:44 -0600

    switched over to https

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 import path from 'path'
+import fs from 'fs'
 import {
   createServer,
-} from 'http'
+} from 'https'
 
 import express from 'express'
 
@@ -17,9 +18,14 @@ import {
   afConnectWebsocketServer,
 } from './redux/actions.js'
 
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/spymaster.mjh.io/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/spymaster.mjh.io/cert.pem'),
+}
+
 const app = express()
 
-const server = createServer(app)
+const server = createServer(options, app)
 
 // Connect websocket server
 store.dispatch(afConnectWebsocketServer(server))
