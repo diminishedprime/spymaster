c7fa640797a04983aa91daf3c7db3c663617eeea
Matt Hamrick <matthewjhamrick@gmail.com>
2019-06-10 22:55:51 -0700

    clean do.

--- api/src/redux/sagas/app.ts
+++ api/src/redux/sagas/app.ts
@@ -1,64 +1,19 @@
 import R from "ramda";
 import uuid4 from "uuid/v4";
 import * as t from "../../types";
-
-import { newGame } from "../initial-state";
+import * as initialState from "../initial-state";
 import * as actions from "../../../../src/redux/actions";
-import {
-  afJoinGame,
-  afSetGameId,
-  afSetGameIds,
-  afSetUserId,
-  afStopTimer,
-  afSetTime,
-  afNextTurn,
-  afSetCardFlipped,
-  afUpdateHintNumber,
-  afForfeit,
-  afUpdateRemoteState,
-  afSetUsername,
-  afSetPage
-} from "../../../../src/redux/actions";
-import { ASSASSIN } from "../../../../src/constants";
-import {
-  cardTeamByCardId,
-  cardsPath,
-  currentTeamPath,
-  hintNumberPath
-} from "../../../src/redux/paths";
+import * as constants from "../../../../src/constants";
+import * as paths from "../../../src/redux/paths";
 import * as serverActions from "../actions";
-import {
-  afRemoveUser,
-  afChangeBackgroundColorServer,
-  afJoinGameServer,
-  afBroadcastActionToUserIds,
-  afBroadcastActionToUserId,
-  afBroadcastActionToAll,
-  afNewGameServer,
-  afRemoveUserFromGame
-} from "../actions";
-import {
-  gamesPath,
-  gameUsersPath,
-  gameByGameIdPath as gameByGameId
-} from "../paths";
+import * as serverPaths from "../paths";
+import * as saga from 'redux-saga/effects';
 
-import {
-  put,
-  all,
-  takeEvery,
-  race,
-  take,
-  call,
-  select
-} from "redux-saga/effects";
-import { FlipCard } from "../../../../src/types";
-
-const pushGameState = function*(gameId: t.GameId) {
-  const remoteState = yield select(R.view(gameByGameId(gameId)));
-  const remoteStateAction = afUpdateRemoteState(remoteState);
-  const userIds: t.UserId[] = R.view(gameUsersPath, remoteState);
-  yield put(afBroadcastActionToUserIds(userIds, remoteStateAction));
+const pushGameState = function* (gameId: t.GameId) {
+  const remoteState = yield saga.select(R.view(serverPaths.gameByGameIdPath(gameId)));
+  const remoteStateAction = actions.afUpdateRemoteState(remoteState);
+  const userIds: t.UserId[] = R.view(serverPaths.gameUsersPath, remoteState);
+  yield saga.put(serverActions.afBroadcastActionToUserIds(userIds, remoteStateAction));
 };
 
 const wait = (millis: number) =>
@@ -66,24 +21,24 @@ const wait = (millis: number) =>
     setTimeout(() => resolve(), millis);
   });
 
-const removeUserFromGame = function*(gameId: t.GameId, userId: t.UserId) {
-  const game = yield select(R.view(gameByGameId(gameId)));
-  const gameUserIds: t.UserId[] = R.view(gameUsersPath, game);
+const removeUserFromGame = function* (gameId: t.GameId, userId: t.UserId) {
+  const game = yield saga.select(R.view(serverPaths.gameByGameIdPath(gameId)));
+  const gameUserIds: t.UserId[] = R.view(serverPaths.gameUsersPath, game);
   if (gameUserIds.indexOf(userId) > -1) {
-    yield put(afRemoveUserFromGame(gameId, userId));
+    yield saga.put(serverActions.afRemoveUserFromGame(gameId, userId));
     yield pushGameState(gameId);
   }
 };
 
-const userDisconnected = function*() {
-  yield takeEvery<any>(t.ServerAsyncActionType.USER_DISCONNECTED, function*({
+const userDisconnected = function* () {
+  yield saga.takeEvery<any>(t.ServerAsyncActionType.USER_DISCONNECTED, function* ({
     userId
   }: t.USER_DISCONNECTEDAction) {
-    yield put(afRemoveUser(userId));
-    const gameIds = yield select(
+    yield saga.put(serverActions.afRemoveUser(userId));
+    const gameIds = yield saga.select(
       R.compose(
         R.keys,
-        R.view(gamesPath)
+        R.view(serverPaths.gamesPath)
       )
     );
     for (let i = 0; i < gameIds.length; i++) {
@@ -93,29 +48,29 @@ const userDisconnected = function*() {
   });
 };
 
-const userConnected = function*() {
-  yield takeEvery<any>(t.ServerAsyncActionType.USER_CONNECTED, function*({
+const userConnected = function* () {
+  yield saga.takeEvery<any>(t.ServerAsyncActionType.USER_CONNECTED, function* ({
     userId
   }: t.USER_CONNECTEDAction) {
-    const setUsername = afSetUsername(userId.substring(0, 8));
-    const setUserId = afSetUserId(userId);
-    const gameIds = yield select(
+    const setUsername = actions.afSetUsername(userId.substring(0, 8));
+    const setUserId = actions.afSetUserId(userId);
+    const gameIds = yield saga.select(
       R.compose(
         R.keys,
-        R.view(gamesPath)
+        R.view(serverPaths.gamesPath)
       )
     );
-    const setGameIds = afSetGameIds(gameIds);
-    yield put(afBroadcastActionToUserId(userId, setGameIds));
-    yield put(afBroadcastActionToUserId(userId, setUsername));
-    yield put(afBroadcastActionToUserId(userId, setUserId));
+    const setGameIds = actions.afSetGameIds(gameIds);
+    yield saga.put(serverActions.afBroadcastActionToUserId(userId, setGameIds));
+    yield saga.put(serverActions.afBroadcastActionToUserId(userId, setUsername));
+    yield saga.put(serverActions.afBroadcastActionToUserId(userId, setUserId));
     /* yield forceUpdateRemoteState()*/
   });
 };
 
-const loseGame = function*(teamThatLost: t.Team) {
-  yield put(afForfeit(teamThatLost));
-  yield put(afStopTimer());
+const loseGame = function* (teamThatLost: t.Team) {
+  yield saga.put(actions.afForfeit(teamThatLost));
+  yield saga.put(actions.afStopTimer());
   /*yield forceUpdateRemoteState()*/
 };
 
@@ -126,21 +81,21 @@ const zeroRemainingCards = (team: t.Team, cards: t.Card[]) => {
   return cardsForTeam.length === 0;
 };
 
-const flipCard = function*() {
-  yield takeEvery<any>(t.ActionType.FLIP_CARD, function*({
+const flipCard = function* () {
+  yield saga.takeEvery<any>(t.ActionType.FLIP_CARD, function* ({
     cardId
   }: t.FlipCard) {
     // Always flip the card, because that's a safe thing to do
-    yield put(afSetCardFlipped(cardId));
+    yield saga.put(actions.afSetCardFlipped(cardId));
     /* yield forceUpdateRemoteState()*/
 
-    const state = yield select(R.identity);
-    const cards: t.Card[] = R.view(cardsPath, state);
-    const teamForCard = R.view(cardTeamByCardId(cardId), state);
-    const currentTeam: t.Team = R.view(currentTeamPath, state);
+    const state = yield saga.select(R.identity);
+    const cards: t.Card[] = R.view(paths.cardsPath, state);
+    const teamForCard = R.view(paths.cardTeamByCardId(cardId), state);
+    const currentTeam: t.Team = R.view(paths.currentTeamPath, state);
 
     const correctCard = currentTeam === teamForCard;
-    const pickedAssassin = teamForCard === ASSASSIN;
+    const pickedAssassin = teamForCard === constants.ASSASSIN;
     const team1done = zeroRemainingCards(t.Team.TEAM_1, cards);
     const team2done = zeroRemainingCards(t.Team.TEAM_2, cards);
 
@@ -152,111 +107,111 @@ const flipCard = function*() {
       yield loseGame(t.Team.TEAM_1);
     } else if (correctCard) {
       // decrease the number of guesses if necessary
-      const numGuesses: t.HintNumber = R.view(hintNumberPath, state);
+      const numGuesses: t.HintNumber = R.view(paths.hintNumberPath, state);
       if (!(numGuesses === "Infinity" || numGuesses === "Zero")) {
         const asNumber = numGuesses - 1;
         if (asNumber === 0) {
-          yield put(afNextTurn());
-          yield put(afStopTimer());
+          yield saga.put(actions.afNextTurn());
+          yield saga.put(actions.afStopTimer());
           /* yield forceUpdateRemoteState()*/
         } else {
-          yield put(afUpdateHintNumber(asNumber));
+          yield saga.put(actions.afUpdateHintNumber(asNumber));
           /* yield forceUpdateRemoteState()*/
         }
       }
     } else {
-      yield put(afStopTimer());
-      yield put(afNextTurn());
+      yield saga.put(actions.afStopTimer());
+      yield saga.put(actions.afNextTurn());
       /* yield forceUpdateRemoteState()*/
     }
   });
 };
 
-const nextTurn = function*() {
-  yield takeEvery<any>(t.ActionType.NEXT_TURN, function*() {
-    yield put(afStopTimer());
+const nextTurn = function* () {
+  yield saga.takeEvery<any>(t.ActionType.NEXT_TURN, function* () {
+    yield saga.put(actions.afStopTimer());
   });
 };
 
-const timer = function*() {
-  yield takeEvery<any>(t.ActionType.START_TIMER, function*() {
+const timer = function* () {
+  yield saga.takeEvery<any>(t.ActionType.START_TIMER, function* () {
     let wasStopped = false;
     let seconds = 60;
-    yield put(afSetTime(seconds));
+    yield saga.put(actions.afSetTime(seconds));
     for (; seconds >= 0; seconds--) {
-      const { stopped } = yield race({
-        stopped: take(t.ActionType.STOP_TIMER),
-        tick: call(wait, 1000)
+      const { stopped } = yield saga.race({
+        stopped: saga.take(t.ActionType.STOP_TIMER),
+        tick: saga.call(wait, 1000)
       });
       if (stopped) {
         wasStopped = true;
         break;
       } else {
-        yield put(afSetTime(seconds));
+        yield saga.put(actions.afSetTime(seconds));
         /* yield forceUpdateRemoteState()*/
       }
     }
     if (!wasStopped) {
-      yield put(afNextTurn());
+      yield saga.put(actions.afNextTurn());
       /* yield forceUpdateRemoteState()*/
     }
   });
 };
 
-const joinGame = function*() {
-  yield takeEvery<any>(t.ActionType.JOIN_GAME, function*({
+const joinGame = function* () {
+  yield saga.takeEvery<any>(t.ActionType.JOIN_GAME, function* ({
     userId,
     gameId
   }: t.JoinGame) {
-    yield put(afJoinGameServer(userId, gameId));
+    yield saga.put(serverActions.afJoinGameServer(userId, gameId));
 
-    yield put(afBroadcastActionToUserId(userId, afSetGameId(gameId)));
-    yield put(
-      afBroadcastActionToUserId(userId, afSetPage(t.Page.GAME_MODE_PICK_TEAM))
+    yield saga.put(serverActions.afBroadcastActionToUserId(userId, actions.afSetGameId(gameId)));
+    yield saga.put(
+      serverActions.afBroadcastActionToUserId(userId, actions.afSetPage(t.Page.GAME_MODE_PICK_TEAM))
     );
     yield pushGameState(gameId);
   });
 };
 
-const changeBackgroundColor = function*() {
-  yield takeEvery<any>(t.ActionType.CHANGE_BACKGROUND_COLOR, function*({
+const changeBackgroundColor = function* () {
+  yield saga.takeEvery<any>(t.ActionType.CHANGE_BACKGROUND_COLOR, function* ({
     gameId,
     team,
     backgroundColor
   }: // TODO figure out a better way to type this. I add gameId and userId to all outgoing actions, but since these are the client types it doesn't include that information.
-  t.ChangeBackgroundColor & { gameId: t.GameId }) {
-    yield put(afChangeBackgroundColorServer(gameId, team, backgroundColor));
+    t.ChangeBackgroundColor & { gameId: t.GameId }) {
+    yield saga.put(serverActions.afChangeBackgroundColorServer(gameId, team, backgroundColor));
     yield pushGameState(gameId);
   });
 };
 
-const newGameSaga = function*() {
-  yield takeEvery<any>(t.ActionType.NEW_GAME_2, function*({
+const newGameSaga = function* () {
+  yield saga.takeEvery<any>(t.ActionType.NEW_GAME_2, function* ({
     userId
   }: t.NewGame2 & { userId: t.UserId }) {
     const gameId = uuid4();
-    const gameState = newGame();
-    yield put(afNewGameServer(gameId, gameState));
+    const gameState = initialState.newGame();
+    yield saga.put(serverActions.afNewGameServer(gameId, gameState));
 
-    const gameIds = yield select(
+    const gameIds = yield saga.select(
       R.compose(
         R.keys,
-        R.view(gamesPath)
+        R.view(serverPaths.gamesPath)
       )
     );
-    yield put(afBroadcastActionToAll(afSetGameIds(gameIds)));
+    yield saga.put(serverActions.afBroadcastActionToAll(actions.afSetGameIds(gameIds)));
 
-    yield put(afJoinGame(gameId, userId));
+    yield saga.put(actions.afJoinGame(gameId, userId));
   });
 };
 
-const setRole = function*() {
-  yield takeEvery(t.ActionType.PICK_ROLE, function*({
+const setRole = function* () {
+  yield saga.takeEvery(t.ActionType.PICK_ROLE, function* ({
     userId,
     ...action
   }: t.PickRole & { userId: t.UserId }) {
-    yield put(serverActions.afBroadcastActionToUserId(userId, action));
-    yield put(
+    yield saga.put(serverActions.afBroadcastActionToUserId(userId, action));
+    yield saga.put(
       serverActions.afBroadcastActionToUserId(
         userId,
         actions.afSetPage(t.Page.GAME_MODE_GAME)
@@ -265,8 +220,8 @@ const setRole = function*() {
   });
 };
 
-export default function*() {
-  yield all([
+export default function* () {
+  yield saga.all([
     changeBackgroundColor(),
     setRole(),
     joinGame(),
