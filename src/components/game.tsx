3fc3263daac7bff1aec1ef83ca8b100db9f20e1a
Matt Hamrick <matthewjhamrick@gmail.com>
2019-06-13 00:39:59 +0000

    migrated game.

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,33 +1,21 @@
 import React from "react";
-import * as t from "../types";
-import R from "ramda";
-import { connect } from "react-redux";
-
 import Board from "./board";
 import Info from "./info";
 import Timer from "./timer";
-import { cardsPath } from "../redux/paths";
+import * as lens from "../redux/lenses";
+import * as actions from "../redux/actions";
 
-interface StateProps {
-  cardIds: t.CardId[];
-}
-
-type AllProps = StateProps;
-
-const mapStateToProps = (state: t.ReduxState): StateProps => {
-  const cardIds = R.keys(R.view(cardsPath, state));
-  return {
-    cardIds
-  };
+const Game: React.FC = () => {
+  const cards = actions.useLensSelector(lens.cards);
+  const cardIds = Object.keys(cards);
+  return (
+    <div>
+      <Timer />
+      <Board cardIds={cardIds} />
+      <Timer />
+      <Info />
+    </div>
+  );
 };
 
-const Game: React.FC<AllProps> = ({ cardIds }) => (
-  <div>
-    <Timer />
-    <Board cardIds={cardIds} />
-    <Timer />
-    <Info />
-  </div>
-);
-
-export default connect(mapStateToProps)(Game);
+export default Game;
