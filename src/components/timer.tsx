852d14577ede7c3025900be1e7e0bb8f340cc7d7
Matt Hamrick <matthewjhamrick@gmail.com>
2019-06-13 00:52:42 +0000

    migrated timer.

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,54 +1,42 @@
 import React from "react";
 import * as t from "./../types";
-import R from "ramda";
 import { connect } from "react-redux";
-
+import * as actions from "../redux/actions";
+import * as lens from "../redux/lenses";
 import { afStartTimer } from "./../redux/actions";
-import { timePath, hintSubmittedPath } from "./../redux/paths";
 
 const timerStyle = {
   display: "flex",
   justifyContent: "cente"
 };
 
-interface StateProps {
-  time: number;
-  disabled: boolean;
-}
-
 interface DispatchProps {
   start: () => void;
 }
 
-type AllProps = DispatchProps & StateProps;
-
-const mapStateToProps = (state: t.ReduxState): StateProps => {
-  const time: number = R.view(timePath, state);
-  const hintSubmitted = R.view(hintSubmittedPath, state);
-  const disabled = !hintSubmitted;
-  return {
-    time,
-    disabled
-  };
-};
+type AllProps = DispatchProps;
 
 const mapDispatchToProps = (dispatch: t.Dispatch): DispatchProps => ({
   start: () => dispatch(afStartTimer())
 });
 
-const Timer: React.FC<AllProps> = ({ time, start, disabled }) => (
-  <div style={timerStyle}>
-    {time ? (
-      <div>Time Remaining: {time} </div>
-    ) : (
-      <button onClick={start} disabled={disabled}>
-        Start Timer
-      </button>
-    )}
-  </div>
-);
+const Timer: React.FC<AllProps> = ({ start }) => {
+  const time = actions.useLensSelector(lens.time);
+  const disabled = !actions.useLensSelector(lens.hintSubmitted);
+  return (
+    <div style={timerStyle}>
+      {time ? (
+        <div>Time Remaining: {time} </div>
+      ) : (
+        <button onClick={start} disabled={disabled}>
+          Start Timer
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default connect(
-  mapStateToProps,
+  undefined,
   mapDispatchToProps
 )(Timer);
